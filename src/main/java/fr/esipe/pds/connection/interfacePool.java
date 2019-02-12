406d6940569b1efd99b8532f0ca15fdd59d9d9c2
ismailelhammoud <46443699+ismailelhammoud@users.noreply.github.com>
2019-02-12 15:52:57 +0100

    Update interfacePool.java

--- src/main/java/fr/esipe/pds/connection/interfacePool.java
+++ src/main/java/fr/esipe/pds/connection/interfacePool.java
@@ -7,10 +7,20 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 
 
@@ -20,79 +30,155 @@ public class interfacePool extends JFrame implements ActionListener
 	    private JButton bouton1;
 	    private JButton bouton2;
 	    private JButton bouton3;
-	    private JButton bouton4;
+	
 	    private JButton bouton5;
 	    private JLabel label1;
 	    private JLabel label2;
 	    private JLabel label3;
-	    private JLabel label4;
-	    private JLabel label5;
 	    
-	    public interfacePool () 
+	    private JLabel label5;
+	    private JTextField entre1, sortie1;
+	    private JTextField entre2, sortie2;
+	    private JTextField entre3, sortie3;
+	    private JTextField entre4, sortie4;
+	    private JTextField entre5, sortie5;
+	    
+	    public interfacePool (JDBCConnectionPool jcp) 
 	    {
-	        setTitle("Interface Pool"); setSize(2000, 720);
+	    	
+	        setTitle("Interface Pool"); setSize(700, 700);
 	        setLocationRelativeTo(null);
 	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        getContentPane().setLayout(new FlowLayout());
 	        getContentPane().setBackground(new Color(200,200,200));
-	        bouton1 = new JButton("SELECT");
+	        bouton1 = new JButton("SELECTIONNER UN OBJET");
 	        bouton1.addActionListener(this);
 	       
+	      
 	       
-	       
-	        bouton2 = new JButton("UPDATE");
+	        bouton2 = new JButton("MODIFIER L'ETAT D'UN OBJET");
 	        bouton2.addActionListener(this);
 	        
 	        
-	        bouton3 = new JButton("DELETE");
+	        bouton3 = new JButton("SUPPRIMER UN OBJET");
 	        bouton3.addActionListener(this);
 	        
-	        bouton4 = new JButton("SELECT");
-	        bouton4.addActionListener(this);
+	       
 	        
-	        bouton5 = new JButton("ADD");
+	        bouton5 = new JButton("AJOUTER UN OBJET");
 	        bouton5.addActionListener(this);
 	        
-	        getContentPane().setLayout(new GridLayout(5,1));
+	        getContentPane().setLayout(new GridLayout(14,1));
 	        
+	       
+	        
+	        entre1 = new JTextField("ENTREZ LE NUMERO D'UN OBJET", 20);
+	        entre1.addActionListener(this);
+	        getContentPane().add(entre1);
+	        sortie1 = new JTextField(20);
+	        sortie1.setEditable(false);
+	        sortie1.setForeground(Color.RED);
+	        getContentPane().add(sortie1);
 	        getContentPane().add(bouton1);
-	        label1 = new JLabel("Sélectionner l'état de l'objet connecté numéro 12");
-	        label1.setForeground(Color.BLACK);
-	        getContentPane().add(label1);
+	        
+	        
+	        entre2 = new JTextField("ENTREZ LE NOUVEL ETAT DE VOTRE OBJET (ON/OFF)", 20);
+	        entre2.addActionListener(this);
+	        getContentPane().add(entre2);
+	        
+	       
+	        entre3 = new JTextField("ENTREZ LE NUMERO DE VOTRE OBJET", 20);
+	        entre3.addActionListener(this);
+	        getContentPane().add(entre3);
+	        
+	        
 	        getContentPane().add(bouton2);
-	        label2 = new JLabel("Modifier l'état de l'objet 12");
-	        label2.setForeground(Color.BLACK);
-	        getContentPane().add(label2);
+	        
+	        entre4 = new JTextField("ENTREZ LE NUMERO DE VOTRE OBJET", 20);
+	        entre4.addActionListener(this);
+	        getContentPane().add(entre4);
+	        
+	        
 	        getContentPane().add(bouton3);
-	        label3 = new JLabel("Supprimer l'objet numéro 10");
-	        label3.setForeground(Color.BLACK);
-	        getContentPane().add(label3);
-	        getContentPane().add(bouton4);
-	        label4 = new JLabel("Sélectionner l'objet connecté numéro 10");
-	        label4.setForeground(Color.BLACK);
-	        getContentPane().add(label4);
+	        
+	        entre5 = new JTextField("ENTREZ L'ETAT DE VOTRE NOUVEL OBJET", 20);
+	        entre5.addActionListener(this);
+	        getContentPane().add(entre5);
+	        
 	        getContentPane().add(bouton5);
-	        label5 = new JLabel("Ajouter l'utilisateur numéro 15");
-	        label5.setForeground(Color.BLACK);
-	        getContentPane().add(label5);
-	        
-	    
-	        
-	       
-	       
-	        
+	     
 	    }
+	     
+	   
+	   
 	    public void actionPerformed(ActionEvent event) 
 	    {
-	        // TODO 
-	   
+	       if (event.getSource()== bouton1)
+	       {
+	    	   String id =entre1.getText();
+	    	   int identifient = Integer.parseInt(id);
+	    	
+	    	   boolean result ;
+			   Connection connection = jcp.getConnection();
+			   Statement ordre = connection.createStatement();
+			   String sql = "Select Etat from Objwhere Id_Objets ="+ identifient;
+			   ResultSet rs = ordre.executeQuery(sql);
+			   while (rs.next())
+			   {
+				   result = rs.getBoolean("Etat");
+			   }
+			   if (result == true){
+				   sortie1.setText("ON");
+			   }
+			   else 
+			   {
+				   sortie1.setText("OFF");
+			   }
+	       }
+	       if (event.getSource()== bouton2)
+	       {
+	    	   String id =entre2.getText();
+	    	   int identifient = Integer.parseInt(id);
+	    	   String etat =entre3.getText();
+	    	   Boolean idEtat = Boolean.parseBoolean(etat);
+	    	
+			   Connection connection = jcp.getConnection();
+			   Statement ordre = connection.createStatement();
+			   String sql = "UPDATE Etat from Objets_Connectes set " + idEtat + "where Id_Objets ="+ identifient;
+			   ordre.executeQuery(sql);
+			   
+	       }
+	       if (event.getSource()== bouton3)
+	       {
+	    	   String id =entre4.getText();
+	    	   int identifient = Integer.parseInt(id);
+	    	
+			   Connection connection = jcp.getConnection();
+			   Statement ordre = connection.createStatement();
+			   String sql = "DELETE FROM Objets_Connectes WHERE Id_Objets="+ identifient;
+			   ordre.executeQuery(sql);
+			  
+			   
+	       }
+	       if (event.getSource()== bouton5)
+	       {
+	    	   String etat =entre5.getText();
+	    	   Boolean identifientEtat = Boolean.parseBoolean(etat);
+	    	
+			   Connection connection = jcp.getConnection();
+			   Statement ordre = connection.createStatement();
+			   String sql = "INSERT INTO Objets_Connectes(Etat) VALUES"+ identifientEtat;
+			   ordre.executeQuery(sql);
+	       }
+	    	
 	    }
-	    
-	  
+	    public static void main (String[] args) {
+			
+	    	JDBCConnectionPool jcp = new JDBCConnectionPool();
+			interfacePool instance = new interfacePool(jcp);
+			instance.setVisible(true);
+	    }
+}
 
-	    	public static void main (String[] args) {
-	    		
-	    		interfacePool instance = new interfacePool();
-	    		instance.setVisible(true);
-	        }
-	    }
+
+
