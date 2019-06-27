907454582cd4a5c42325cc1a01d26690bd670b4f
Nawfal Laiouar <nawfal1.laiouar@etu.u-pec.fr>
2019-06-28 00:40:08 +0200

    version a Merge pour demo

---
+++ ehpaddecisionServ/src/main/java/fr/esipe/pds/ehpaddecision/sensors/InterfaceSensorDAO.java
@@ -0,0 +1,15 @@
+package fr.esipe.pds.ehpaddecision.sensors;
+
+import java.util.List;
+
+import fr.esipe.pds.ehpaddecision.principales.Sensors;
+import fr.esipe.pds.ehpaddecision.principales.Temperatures_Sensors;
+
+public interface InterfaceSensorDAO {
+	void update (Sensors sensors);
+	void delete (Sensors sensors);
+	
+	
+	
+
+}
